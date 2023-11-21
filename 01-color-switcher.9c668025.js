cdbfc69a48c9b64d798f0791e2e7731b7381df5c
khrom22 <107980144+khrom22@users.noreply.github.com>
2023-11-21 23:28:35 +0000

    Deploying to gh-pages from @ khrom22/goit-js-hw-09@71048f4fc9cf9b80e771ee3301741370935b7e5c 🚀

---
+++ 01-color-switcher.9c668025.js
@@ -0,0 +1,2 @@
+const t={btnStart:document.querySelector("button[data-start]"),btnStop:document.querySelector("button[data-stop]"),body:document.querySelector("body")};t.btnStop.disabled=!0,t.btnStart.addEventListener("click",(function(){t.btnStart.disabled=!0,t.btnStop.disabled=!1;setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),t.btnStop.addEventListener("click",(function(){t.btnStart.disabled=!1,t.btnStop.disabled=!0,clearInterval(int)}));
+//# sourceMappingURL=01-color-switcher.9c668025.js.map
