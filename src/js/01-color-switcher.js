0d02a1ef266bdf2bbe09e2a08c005f7912579b5e
Roman Khomyn <107980144+khrom22@users.noreply.github.com>
2023-11-22 00:55:09 +0200

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+    btnStart: document.querySelector('button[data-start]'),
+    btnStop: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body')
+}
+refs.btnStop.disabled = true
+refs.btnStart.addEventListener('click', changeColor)
+refs.btnStop.addEventListener('click', changeColorStop)
 
+
+function changeColor() {
+    refs.btnStart.disabled = true,
+        refs.btnStop.disabled = false,
+        int = setInterval(() => {
+            refs.body.style.backgroundColor = getRandomHexColor()
+        }, 1000
+        )
+}
+function changeColorStop() {
+    refs.btnStart.disabled = false,
+        refs.btnStop.disabled = true,
+        clearInterval(int)
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
